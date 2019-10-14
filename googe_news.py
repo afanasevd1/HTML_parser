750a2b5c635e76899e97e83da29c88635d8949a4
afanasevd1 <afanasevd1@gmail.com>
2019-10-14 23:17:13 +0500

    Refactor2

--- googe_news.py
+++ googe_news.py
@@ -15,15 +15,14 @@ class Scraper:
     def scrape(self):
         html = urllib.request.urlopen(self.site).read()
         parser = "html.parser"
-        url_file = open(self.path, "w")
-        for tag in BeautifulSoup(html, parser).find_all(class_="DY5T1d"):
-            url_str = str(tag)
-            if url_str is None:
-                continue
-            url_file.write("<p> {}) - ".format(self.news_num) + url_str + "</p>")
-            self.news_num += 1
-        url_file.write('\n<base href="https://news.google.ru/"</>')
-        url_file.close()
+        with open(self.path, "w") as url_file:
+            for tag in BeautifulSoup(html, parser).find_all(class_="DY5T1d"):
+                url_str = str(tag)
+                if url_str is None:
+                    continue
+                url_file.write("<p> {}) - ".format(self.news_num) + url_str + "</p>")
+                self.news_num += 1
+            url_file.write('\n<base href="https://news.google.ru/"</>')
 
 
 news = "https://news.google.ru/"
